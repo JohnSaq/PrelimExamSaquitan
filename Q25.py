3305fc383681753c9c1ffaf4e188b206bab8aa63
JohnSaq <142981946+JohnSaq@users.noreply.github.com>
2023-10-24 07:01:28 +0000

    Update Q25.py

--- Q25.py
+++ Q25.py
@@ -1,30 +1,17 @@
 import math
 
-class sqr():
+class square():
+    def __init__(self,area):
+        self.a = area
 
-    def __init__(self, square):
-        self.square = square
+    def area(self):
+        return (self.a * self.a)
 
-    def areas(self):
-        area = (self.square*self.square)
-        return area
+    def perimeter(self):
+        return(self.a*4)
 
-    def perimeter():
-        per = (self.square*4)
-        return per
-
-    def printres(self):
-        myarea = self.areas
-        myper = self.per
-        print("The area of the square is: ",+str(myarea)+"and the perimeter is: ",+str(myper))
-        return
-#if __name__ == '__main__':
-    
-s = input(print("please insert size of the square: "))
-
-
-sqr1 = sqr(s)
-sqr1.printres
-
-
-print("The area of the square is: 4 and the perimeter is: 8")
+if __name__=='__main__':
+    s = square(5)
+    print("The side of the square is 5")
+    print("The area of Square is ", + s.area())
+    print("The perimeter of Square is ", + s.perimeter())
